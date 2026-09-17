f32ec8099c74e5740a54f031c8ebc7b7aa2714b8
Blujaxs676 <Blujaxs676@gmail.com>
2026-09-17 00:11:31 -0500

    Update Reservation.h added expie stuff for temporary testing.

--- Reservation.h
+++ Reservation.h
@@ -7,8 +7,8 @@ private:
   Reservation* next;
   Reservation* prev;
 public:
-  //Constructor. Defaults next and prev to nullptr.
-  Reservation();
+  //Constructor. Defaults next and prev to nullptr. Expie value part is very temporary.
+  Reservation(int Expievalue);
 
   //Getter Setter methods for next.
   Reservation* getNext() const;
@@ -16,7 +16,7 @@ public:
 
   //Getter Setter methods for prev.
   Reservation* getPrev() const;
-  void getPrev(Reservation* prev);
+  void setPrev(Reservation* prev);
 
   //Getter for expieValue. No setter. Also temporary, seriously, remove me later.
   int getExpie() const;
