0912347bca7e017517cfaa831b3d67d2a40f91f0
Blujaxs676 <Blujaxs676@gmail.com>
2026-09-16 23:37:11 -0500

    Update Reservation.h, changed to doubly linked.

--- Reservation.h
+++ Reservation.h
@@ -4,14 +4,19 @@
 class Reservation {
 private:
   int expieValue; //temporary. Remove after experimenting.
-  Reservation* next; 
+  Reservation* next;
+  Reservation* prev;
 public:
-  //Constructor. Defaults next to nullptr.
+  //Constructor. Defaults next and prev to nullptr.
   Reservation();
 
   //Getter Setter methods for next.
   Reservation* getNext() const;
   void setNext(Reservation* next);
+
+  //Getter Setter methods for prev.
+  Reservation* getPrev() const;
+  void getNext(Reservation* prev);
 
   //Getter for expieValue. No setter. Also temporary, seriously, remove me later.
   int getExpie() const;
