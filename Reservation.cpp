5cebb1be15cd69b96fe47229323b313f629421a0
Blujaxs676 <Blujaxs676@gmail.com>
2026-09-16 23:14:39 -0500

    Update Reservation.cpp demo
    Really easy code stuffs.

--- Reservation.cpp
+++ Reservation.cpp
@@ -1,1 +1,18 @@
-//wip
+#include "Reservation.h"
+
+Reservation::Reservation() : expieValue(0), next(nullptr) {}
+
+Reservation* Reservation::getNext() const
+{
+   return next;
+}
+
+void Reservation::setNext(Reservation* next)
+{
+  this->next = next;
+}
+
+int Reservation::getExpie() const
+{
+  return expieValue;
+}
