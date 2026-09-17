d60bfa7c2d81cc9271e3a9de2d9f4d073941837e
Blujaxs676 <Blujaxs676@gmail.com>
2026-09-16 23:05:44 -0500

    Update Reservation.h demo
    demo because I dunno if this is good enough for the cpp files.

--- Reservation.h
+++ Reservation.h
@@ -1,1 +1,19 @@
-//wip
+#ifndef RESERVE_H
+#define RESERVE_H
+
+class Reservation {
+private:
+  int expieValue; //temporary. Remove after experimenting.
+  Reservation* next; 
+public:
+  //Constructor. Defaults next to nullptr.
+  Reservation();
+
+  //Getter Setter methods for next.
+  Reservation* getNext() const;
+  void setNext(Reservation* next);
+
+  //Getter for expieValue. No setter. Also temporary, seriously, remove me later.
+  int getExpie() const;
+};
+#endif
