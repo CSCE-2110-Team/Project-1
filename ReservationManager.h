583dffa97b312b41865e6a5627ee6841fcfcc602
Blujaxs676 <Blujaxs676@gmail.com>
2026-09-16 23:05:28 -0500

    Update ReservationManager.h demo
    May or may not be good 'nough for now. Not sure.

--- ReservationManager.h
+++ ReservationManager.h
@@ -1,1 +1,29 @@
-//wip too lololol
+#ifndef RESERVEMANAGER_H
+#define RESERVEMANAGER_H
+#include "Reservation.h"
+
+class ReservationManager {
+private:
+  Reservation* head;
+  int length;
+public:
+  // Constructor defaults to nullptr for head and length 0.
+  ReservationManager();
+
+  // Adds a reservation at the pointer
+  void addReserve(Reservation* reserve, Reservation* at);
+  
+  // Adds a reservation at the head.
+  void addReserve(Reservation* reserve);
+  
+  // Removes a reservation at the head.
+  void removeReserve();
+
+  // Removes a reservation by pointer.
+  void removeReserve(Reservation* reserve);
+
+  // Finds a reservation based on expieValue, currently. Prone to change. For ctrl+f purposes, temporary.
+  Reservation* findReserve(int expieValue);
+};
+
+#endif
